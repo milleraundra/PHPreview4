7fb69fec51d28ddde909e934faf16d863ed40ba0
Aundra Miller <miller.aundra@gmail.com>
2016-03-04 08:52:31 -0800

    create save and getAll methods in Store.php

--- src/Store.php
+++ src/Store.php
@@ -62,6 +62,27 @@
             return $this->id;
         }
 
+        function save()
+        {
+            $GLOBALS['DB']->exec("INSERT INTO stores (store_name, street, city, state) VALUES ('{$this->getStoreName()}', '{$this->getStreet()}', '{$this->getCity()}', '{$this->getState()}');");
+            $this->id = $GLOBALS['DB']->lastInsertId();
+        }
+
+        static function getAll()
+        {
+            $returned_stores = $GLOBALS['DB']->query("SELECT * FROM stores");
+            $stores = array();
+            foreach($returned_stores as $store) {
+                $store_name = $store['store_name'];
+                $street = $store['street'];
+                $city = $store['city'];
+                $state= $store['state'];
+                $id = $store['id'];
+                $new_store = new Store($store_name, $street, $city, $state, $id);
+                array_push($stores, $new_store);
+            }
+            return $stores;
+        }
 
     }
 
