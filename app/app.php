9123d71c317bee309273759fd69e4a36ffd3163f
Aundra Miller <miller.aundra@gmail.com>
2016-03-04 12:12:06 -0800

    add route to create a brand on a single store's page

--- app/app.php
+++ app/app.php
@@ -16,22 +16,45 @@
     ));
     // use Symfony\Component\HttpFoundation\Request;
     // Request::enableHttpMethodParameterOverride()
-
+//Home Page
     $app->get("/", function() use ($app) {
         return $app['twig']->render('index.html.twig', array('all_stores' => Store::getAll(), 'all_brands' => Brand::getAll()));
     });
 
+//create store on index page
     $app->post("/create/store", function() use ($app) {
         $new_store = new Store($_POST['store_name'], $_POST['street'], $_POST['city'], $_POST['state']);
         $new_store->save();
         return $app['twig']->render('index.html.twig', array('all_stores' => Store::getAll(), 'all_brands' => Brand::getAll()));
     });
-
+//create brand on index page
     $app->post("/create/brand", function() use ($app) {
         $new_brand = new Brand($_POST['brand_name']);
         $new_brand->save();
         return $app['twig']->render('index.html.twig', array('all_stores' => Store::getAll(), 'all_brands' => Brand::getAll()));
     });
+//view store page
+    $app->get("/store/{id}", function($id) use ($app) {
+        $store = Store::find($id);
+        $store_brands = $store->getBrands();
+        $other_brands = Brand::getAll();
+        return $app['twig']->render('store.html.twig', array('store' => $store, 'match_brands' => $store_brands, 'nonmatch_brands' => $other_brands));
+    });
+//add a brand to a store
+    // $app->post("/store/{id}/add/brand/{id}")
+
+//create brand on store page
+    $app->post("/store/{id}/create/brand", function($id) use ($app) {
+        $store = Store::find($id);
+        $new_brand = new Brand($_POST['brand_name']);
+        $new_brand->save();
+        $store_brands = $store->getBrands();
+        $other_brands = Brand::getAll();
+        return $app['twig']->render('store.html.twig', array('store' => $store, 'match_brands' => $store_brands, 'nonmatch_brands' => $other_brands));
+    });
+
+//view brand page
+    // $app->get("/brand/{id}")
 
     return $app;
 
--- src/Store.php
+++ src/Store.php
@@ -123,6 +123,7 @@
         function delete()
         {
             $GLOBALS['DB']->exec("DELETE FROM stores WHERE id = {$this->getId()}");
+            $GLOBALS['DB']->exec("DELETE FROM stores_brands WHERE store_id = {$this->getId()};");
         }
 
         function addBrand($brand)
