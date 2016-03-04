58c4eb2008543788a2427f319a193522e3420f4c
Aundra Miller <miller.aundra@gmail.com>
2016-03-04 08:46:15 -0800

    add save and getAll tests to StoreTest

--- tests/StoreTest.php
+++ tests/StoreTest.php
@@ -17,13 +17,14 @@
         function test_allGetters()
         {
             $store_name = "Payless";
-            $street = "392 SW Washington St."
+            $street = "392 SW Washington St.";
             $city = "Oregon City";
             $state = "OR";
-            $test_store = new Store($store_name, $street, $city, $state, 3);
+            $id = 3;
+            $test_store = new Store($store_name, $street, $city, $state, $id);
 
             $result1 = $test_store->getStoreName();
-            $restul2 = $test_store->getStreet();
+            $result2 = $test_store->getStreet();
             $result3 = $test_store->getCity();
             $result4 = $test_store->getState();
             $result5 = $test_store->getId();
@@ -33,6 +34,42 @@
             $this->assertEquals($city, $result3);
             $this->assertEquals($state, $result4);
             $this->assertEquals($id, is_numeric($result5));
+        }
+
+        function test_save()
+        {
+            $store_name = "Payless";
+            $street = "392 SW Washington St.";
+            $city = "Oregon City";
+            $state = "OR";
+            $test_store = new Store($store_name, $street, $city, $state, null);
+
+
+            $test_store->save();
+            $result = Store::getAll();
+
+            $this->assertEquals([$test_store], $result);
+        }
+
+        function test_getAll()
+        {
+            $store_name = "Payless";
+            $street = "392 SW Washington St.";
+            $city = "Oregon City";
+            $state = "OR";
+            $test_store = new Store($store_name, $street, $city, $state, null);
+            $test_store->save();
+
+            $store_name = "Macy's";
+            $street = "180 SW Washington St.";
+            $city = "Clackamas";
+            $state = "OR";
+            $test_store2 = new Store($store_name, $street, $city, $state, null);
+            $test_store2->save();
+
+            $result = Store::getAll();
+
+            $this->assertEquals([$test_store, $test_store2], $result);
         }
     }
 
